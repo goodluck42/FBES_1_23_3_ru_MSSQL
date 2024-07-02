cf22833cece79b08546b21d85d8b0229cd2f64c5
Alex <xrlogic1@gmail.com>
2024-07-02 19:25:51 +0400

    Views

---
+++ console_8.sql
@@ -0,0 +1,21 @@
+-- SELECT FirstName, LastName
+-- FROM Teachers
+
+
+-- CREATE VIEW [TeachersData] AS
+-- SELECT FirstName, LastName
+-- FROM Teachers
+
+-- ALTER VIEW [TeachersData] AS
+-- SELECT FirstName, LastName, IQ
+-- FROM Teachers
+
+-- INSERT INTO Teachers
+-- VALUES ('Aleksandr', 'Malomed', 999)
+
+-- DELETE FROM TeachersData
+-- WHERE LastName = 'Malomed'
+
+-- SELECT *
+-- FROM TeachersData
+
